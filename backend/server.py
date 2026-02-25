83a9b7d50cbd5f298c2e980b54c28f527ea6a945
Bhaumik senwal <bhaumiksenwal99@gmail.com>
2026-02-25 22:58:18 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, APIRouter, HTTPException, Depends, status
 from fastapi.middleware.cors import CORSMiddleware  # <--- THIS WAS MISSING
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from dotenv import load_dotenv
-from motor.motor_asyncio import AsyncIOMotorClient
+from motor.motor_asyncio import AsyncIOMotorClient 
 import os
 import logging
 from pathlib import Path
